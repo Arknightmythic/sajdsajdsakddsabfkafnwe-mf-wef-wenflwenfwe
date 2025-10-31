9f590f86f4497ac9926b49114520fda6500ebff8
A-Jum <kursirusakbanget@gmail.com>
2025-10-31 14:42:00 +0700

    add env

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{os.Getenv("ALLOWED_ORIGINS")},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
