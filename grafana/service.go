736aab922be4c3f6b8289b68c5399da115aae8b7
albertioh <renokristiardy@gmail.com>
2025-11-24 18:14:42 +0700

    turn on grafana dashboard

--- grafana/service.go
+++ grafana/service.go
@@ -21,10 +21,8 @@ func NewGrafanaService(redisClient *redis.Client) *GrafanaService {
 	}
 }
 
-
 func (s *GrafanaService) GenerateEmbedURL(req *GenerateEmbedRequest) (string, error) {
 	var baseURL string
-	var err error
 
 	
 	switch req.Category {
@@ -47,41 +45,45 @@ func (s *GrafanaService) GenerateEmbedURL(req *GenerateEmbedRequest) (string, er
 		return "", fmt.Errorf("grafana embed URL for category '%s' is not set in .env", req.Category)
 	}
 
-	
 	parsedURL, err := url.Parse(baseURL)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse base URL from .env: %w", err)
 	}
 
-	
 	queryParams := parsedURL.Query()
+	queryParams.Set("kiosk", "true")
 	queryParams.Set("theme", "light")
-	queryParams.Set("refresh", "5s") 
-
-	
-	if req.Category == "custom" {
-		fromMs, err := parseDateToMillis(req.StartDate)
+	queryParams.Set("refresh", "5s")
+	queryParams.Set("timezone", "browser")
+	switch req.Category {
+	case "daily":
+		
+		queryParams.Set("from", "now/d")
+		queryParams.Set("to", "now")
+	case "monthly":
+		
+		queryParams.Set("from", "now/M")
+		queryParams.Set("to", "now")
+	case "yearly":
+		
+		queryParams.Set("from", "now/y")
+		queryParams.Set("to", "now")
+	case "custom":
+		fromTime, toTime, err := parseCustomDatesToUTC(req.StartDate, req.EndDate)
 		if err != nil {
-			return "", fmt.Errorf("invalid start_date format: %w", err)
+			return "", err
 		}
-		toMs, err := parseDateToMillis(req.EndDate)
-		if err != nil {
-			return "", fmt.Errorf("invalid end_date format: %w", err)
-		}
-
-		queryParams.Set("from", fmt.Sprintf("%d", fromMs))
-		queryParams.Set("to", fmt.Sprintf("%d", toMs))
+		queryParams.Set("from", fromTime)
+		queryParams.Set("to", toTime)
 	}
 
 	parsedURL.RawQuery = queryParams.Encode()
 	finalGrafanaURL := parsedURL.String()
 
-	
 	token := util.RandString(32)
 	key := "grafana_embed_token:" + token
 	ctx := context.Background()
 
-	
 	err = s.redis.Set(ctx, key, finalGrafanaURL, 1*time.Minute).Err()
 	if err != nil {
 		return "", fmt.Errorf("failed to store embed token in redis: %w", err)
@@ -91,12 +93,20 @@ func (s *GrafanaService) GenerateEmbedURL(req *GenerateEmbedRequest) (string, er
 }
 
 
-func parseDateToMillis(dateString string) (int64, error) {
-	t, err := time.Parse("2006-01-02", dateString)
+func parseCustomDatesToUTC(startDateStr, endDateStr string) (string, string, error) {
+	layout := "2006-01-02"
+	loc := time.FixedZone("WIB", 7*60*60)
+	tStart, err := time.ParseInLocation(layout, startDateStr, loc)
 	if err != nil {
-		return 0, err
+		return "", "", fmt.Errorf("invalid start_date format: %w", err)
 	}
 	
-	tStartOfDay := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-	return tStartOfDay.Unix() * 1000, nil
+	tEnd, err := time.ParseInLocation(layout, endDateStr, loc)
+	if err != nil {
+		return "", "", fmt.Errorf("invalid end_date format: %w", err)
+	}
+	tEnd = time.Date(tEnd.Year(), tEnd.Month(), tEnd.Day(), 23, 59, 59, 0, loc)
+	isoLayout := "2006-01-02T15:04:05.000Z"
+
+	return tStart.UTC().Format(isoLayout), tEnd.UTC().Format(isoLayout), nil
 }
